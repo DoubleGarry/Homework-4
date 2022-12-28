afa7c64dcccf0b62b16e41e7855b23d588ec5941
semenikhin <garry@nextmail.ru>
2022-12-28 21:12:20 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int age = 20;
+        if (age >= 18) {
+            System.out.print("Если возраст человека равен " + age + " то он совершеннолетний");
+        }
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + " то нужно немного подождать");
+        }
     }
 }
